506cc62c86512f22e48d0be3bd4f2e4569d70b45
tadasnik <tadas.nik@gmail.com>
2023-10-01 07:44:18 +0100

    siteInputs writable, removed not used

--- src/lib/shared/stores/inputs.js
+++ src/lib/shared/stores/inputs.js
@@ -1,17 +1,4 @@
 import { writable } from 'svelte/store'
 import { inputNodes } from '$lib/data/inputNodes.js'
 
-const initKeys = [
-	'site.moisture.dead.category',
-	'site.moisture.live.category',
-	'site.slope.steepness.degrees',
-	'site.wind.speed.atMidflame'
-]
-
-const initSiteInputs = {}
-initKeys.forEach((item) => {
-	initSiteInputs[item] = inputNodes[item].value
-})
-
-export const requiredInputs = writable(initKeys)
-export const siteInputs = writable(initSiteInputs)
+export const siteInputs = writable(inputNodes)

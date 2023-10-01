77472843cd53796283467b3d179b08eb30c43f0a
tadasnik <tadas.nik@gmail.com>
2023-10-01 07:41:24 +0100

    inputs work with siteInputs

--- src/lib/components/Inputs.svelte
+++ src/lib/components/Inputs.svelte
@@ -1,44 +1,44 @@
 <script>
-  import { SlideToggle } from '@skeletonlabs/skeleton';
+  import { Accordion, AccordionItem, SlideToggle } from '@skeletonlabs/skeleton';
 	import RangeSlider from 'svelte-range-slider-pips';
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
-	import { requiredInputs } from '$lib/shared/stores/inputs.js';
+  import { siteInputs } from '$lib/shared/stores/inputs.js'
+	import { requiredSiteInputs } from '$lib/shared/stores/modelStore.js';
 </script>
 
 <Accordion>
-	{#each Object.keys($requiredInputs) as key}
+	{#each Object.keys($requiredSiteInputs) as key}
 		<AccordionItem>
 			<svelte:fragment slot="summary">
 				<div class="flex flex-row">
-					<div class="basis-3/4">{$requiredInputs[key].label}</div>
+					<div class="basis-3/4">{$siteInputs[key].label}</div>
 					<div class="basis-1/4">
-						<strong>{$requiredInputs[key].value}</strong>
-						{$requiredInputs[key].units}
+						<strong>{$siteInputs[key].value}</strong>
+						{$siteInputs[key].units}
 					</div>
 				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="content">
 				<SlideToggle name="slider-example" size="sm">Range</SlideToggle>
-				{#if $requiredInputs[key].value.length > 1}
+				{#if $siteInputs[key].value.length > 1}
 					<RangeSlider
 						range
 						float
 						hoverable
 						first="label"
 						last="label"
-						min={$requiredInputs[key].min}
-						max={$requiredInputs[key].max}
-						step={$requiredInputs[key].step}
-						bind:values={$requiredInputs[key].value}
+						min={$siteInputs[key].min}
+						max={$siteInputs[key].max}
+						step={$siteInputs[key].step}
+						bind:values={$siteInputs[key].value}
 					/>
 				{:else}
 					<RangeSlider
 						float
 						hoverable
-						min={$requiredInputs[key].min}
-						max={$requiredInputs[key].max}
-						step={$requiredInputs[key].step}
-						bind:values={$requiredInputs[key].value}
+						min={$siteInputs[key].min}
+						max={$siteInputs[key].max}
+						step={$siteInputs[key].step}
+						bind:values={$siteInputs[key].value}
 					/>
 				{/if}
 			</svelte:fragment>

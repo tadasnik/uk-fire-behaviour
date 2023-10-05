8b804f6d529b1b086febe67b8b36d720e5a64a38
tadasnik <tadas.nik@gmail.com>
2023-10-05 06:50:33 +0100

    removing unused

--- src/lib/shared/stores/fuels.js
+++
@@ -1,8 +0,0 @@
-import { writable, derived } from 'svelte/store';
-
-export const selectedFuels = writable(["gs1", "sh4"]);
-
-export const selectedFuelsInput = derived(selectedFuels, ($selectedFuels) => {
-  const fuelsInputObject = { "surface.primary.fuel.model.catalogKey": $selectedFuels }
-  return fuelsInputObject
-})
--- src/lib/shared/stores/outputs.js
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const selectedOutputs = writable(['surface.weighted.fire.spreadRate']);
